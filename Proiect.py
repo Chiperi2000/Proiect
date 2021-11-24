e7d1b085d589a3f37e684c854649ecef519336c7
Chiperi2000 <iulianchiperi2000@gmail.com>
2021-11-24 10:48:41 +0200

    commit

--- Proiect.py
+++ Proiect.py
@@ -4,14 +4,14 @@ from glob import glob
 import time
 import speech_recognition as sr
 from random import shuffle
-from time import ctime, daylight
 import webbrowser
 from playsound import playsound
 import random
 from gtts import gTTS
+import datetime
+import wikipedia 
 
 inregistrare = sr.Recognizer()
-
 
 def inregistrare_audio(caută=False):
     with sr.Microphone() as microfon:
@@ -21,7 +21,7 @@ def inregistrare_audio(caută=False):
         inregistrare.adjust_for_ambient_noise(microfon, duration=1)
         voce_inregistrata = ''
         try:
-            date_audio = inregistrare.listen(microfon, 10, 3)
+            date_audio = inregistrare.listen(microfon, 10, 10)
             voce_inregistrata = inregistrare.recognize_google(date_audio, language='ro-Ro')
             print(voce_inregistrata)
         except sr.UnknownValueError:
@@ -29,15 +29,6 @@ def inregistrare_audio(caută=False):
         except sr.RequestError:
             voce_bot('Momentan indisponibil!')
         return voce_inregistrata
-
-def voce_bot(mesaj_scris):
-    text = gTTS(text=mesaj_scris, lang='ro')
-    r = random.randint(1, 100000)
-    audio_f = 'fisier-' + str(r) + '.mp3'
-    text.save(audio_f)
-    playsound(audio_f)
-    print(mesaj_scris)
-    os.remove(audio_f)
 
 def inregistrare_joc():
     with sr.Microphone() as microfon:
@@ -52,6 +43,16 @@ def inregistrare_joc():
     except sr.RequestError:
         print('Momentan indisponibil!')
     return text
+
+def voce_bot(mesaj_scris):
+    text = gTTS(text=mesaj_scris, lang='ro')
+    r = random.randint(1, 100000)
+    audio_f = 'fisier-' + str(r) + '.mp3'
+    text.save(audio_f)
+    playsound(audio_f)
+    print(mesaj_scris)
+    os.remove(audio_f)
+
 
 def locatie(locatie_steag):
     fereastra_joc = pygame.display.set_mode((400, 500))
@@ -68,29 +69,31 @@ def locatie(locatie_steag):
         fereastra_joc.blit(imagine, (50, 50))
         pygame.display.update()
         if inregistrare_joc() == steaguri[steag].split("\\")[1]:
-            print('Foarte bine!\n---------------------\n\n')
+            voce_bot('Foarte bine!\n---------------------\n\n')
             punctaj += 1
         else:
-            print("Raspuns gresit!")
+            voce_bot("Raspuns gresit!")
             print("Raspuns corect:", steaguri[steag].split("\\")[1])
         print(f"Scorul tău este: {punctaj}/{nr_cuvinte}" )
-
+    voce_bot(f"Scorul tău este: {punctaj} din {nr_cuvinte}" )
 
 def raspunsuri(voce_inregistrata):
-    if 'cine ești tu' in voce_inregistrata:
-        voce_bot("Eu sunt asistentul tău personal!")
+    if  str.lower(voce_inregistrata) == 'cât este ceasul' or str.lower(voce_inregistrata) == 'ce oră este':
+        file_time = datetime.datetime.fromtimestamp(os.path.getmtime(__file__))
+        voce_bot(file_time.strftime("%H:%M"))
+    
+    if  str.lower(voce_inregistrata) == 'în ce zi suntem' or str.lower(voce_inregistrata) == 'ce data este astăzi':
+        file_time = datetime.datetime.fromtimestamp(os.path.getmtime(__file__))
+        voce_bot(file_time.strftime("%d %m %Y"))
 
-    if 'cât este ceasul' in voce_inregistrata:
-        voce_bot(ctime())
-
-    if 'caută' in voce_inregistrata:
+    if 'caută' in str.lower(voce_inregistrata):
         caută = inregistrare_audio('Ce vrei sa caut?')
         url = 'https://google.ro/search?q=' + caută
         webbrowser.get().open(url)
         print('Rezultate gasite pentru: ' + caută)
 
-    if 'Vreau să mă joc' in voce_inregistrata:
-        joc = inregistrare_audio('Ce vrei sa te joci?\n1.Ghicește steagul')
+    if 'vreau să mă joc' in str.lower(voce_inregistrata):
+        joc = inregistrare_audio('Ce vrei să te joci?\n Alege din următoarele jocuri: Ghicește steagul')
         if joc == 'Ghicește steagul':
             locatie_steag = inregistrare_audio('Steaguri din regiunea:\n 1.Europa \n 2.America de Nord \n 3.America de Sud')
             while locatie_steag == 'Europa':
@@ -108,4 +111,3 @@ time.sleep(1)
 voce_bot('Cu ce te pot ajuta?')
 voce_inregistrata = inregistrare_audio()
 raspunsuri(voce_inregistrata)
-  
